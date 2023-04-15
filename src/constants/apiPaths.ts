2b341d711950b05577489067c452ef7bb9383989
Aliaksandr Shautsou <a.shautsou@sap.com>
2023-04-15 18:55:29 +0300

    added csv upload

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://dgsvd3k2ia.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://dgsvd3k2ia.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://dgsvd3k2ia.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://n4rlmy5ib7.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://dgsvd3k2ia.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://dgsvd3k2ia.execute-api.us-east-1.amazonaws.com/dev",
 };
