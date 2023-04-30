94563816cf2f990f1fecc12178ed79fbcca5cd63
Aliaksandr Shautsou <a.shautsou@sap.com>
2023-04-30 10:13:46 +0300

    added import csv authorization

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 type CSVFileImportProps = {
   url: string;
@@ -25,23 +25,41 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
 
   const uploadFile = async () => {
     console.log("uploadFile to", url);
+    const authorization_token = localStorage.getItem("authorization_token");
 
-    // Get the presigned URL
-    const response = await axios({
-      method: "GET",
-      url,
-      params: {
-        name: encodeURIComponent((file as any)?.name),
-      },
-    });
-    console.log("File to upload: ", (file as any)?.name);
-    console.log("Uploading to: ", response.data?.url);
-    const result = await fetch(response.data?.url, {
-      method: "PUT",
-      body: file,
-    });
-    console.log("Result: ", result);
-    // setFile("");
+    try {
+      // Get the presigned URL
+      const response = await axios({
+        method: "GET",
+        url,
+        params: {
+          name: encodeURIComponent((file as any)?.name),
+        },
+        headers: { Authorization: `Basic ${authorization_token}` },
+      });
+      console.log("File to upload: ", (file as any)?.name);
+      console.log("Uploading to: ", response.data?.url);
+      const result = await fetch(response.data?.url, {
+        method: "PUT",
+        body: file,
+      });
+      console.log("Result: ", result);
+      // setFile("");
+    } catch (error) {
+      console.error('ERROR: ', error);
+      const errorResponse = (error as AxiosError)?.response;
+      if (errorResponse) {
+        const status = errorResponse.status;
+
+        if (status === 400) {
+          alert((errorResponse.data as any)?.data);
+        }
+        if (status === 403 || status === 401) {
+          alert((errorResponse.data as any)?.message);
+        }
+      }
+      return Promise.reject(error);
+    }
   };
   return (
     <Box>
