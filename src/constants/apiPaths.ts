ac299d951ea203fe80164b752c55485720085c5e
Aliaksandr Shautsou <a.shautsou@sap.com>
2023-05-06 16:56:51 +0300

    task 8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://dgsvd3k2ia.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://dgsvd3k2ia.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://4ltg2yi7b5.execute-api.us-east-1.amazonaws.com/dev",
   import: "https://n4rlmy5ib7.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://dgsvd3k2ia.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://dgsvd3k2ia.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://4ltg2yi7b5.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
